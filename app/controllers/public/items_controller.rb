26aa003a0dfb8dc0ce4b1a42ee23e5f63f1ff1f7
pe-shun <shunpepe@gmail.com>
2022-09-01 01:52:21 +0000

    cake

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,33 @@
 class Public::ItemsController < ApplicationController
 
-def index
-end
+    def index
+     @item = Item.new
+     @items = Items.all
+     @user = current_user
+    end
 
-def edit
-end
+    def edit
+    end
 
+    def create
+     @itmes = itmes.new(book_params)
+     @itmes.user_id = current_user.id
+      if @items.save
+       redirect_to book_path(@items.id)
+      else
+     @items = current_user
+     @items = Items.all
+       render :index
+      end
+    end
+
+    def update
+      @items = Items.find(params[:id])
+        if @items.update(items_params)
+         redirect_to items_path
+        else
+         render :edit_item
+        end
+    end
 
 end
